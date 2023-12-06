af16d279be7a80fa9c4d85736d57d899bc62654c
Izadora Braga <izadora_abraga@outlook.com>
2023-12-06 18:01:22 -0300

    requisito 1 finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,17 @@
 import React from 'react';
-import { Provider } from 'react-redux';
 import { Routes, Route } from 'react-router-dom';
-import { composeWithDevTools } from '@redux-devtools/extension';
-import { legacy_createStore as createStore } from 'redux';
-import rootReducer from './redux/reducers';
 import Login from './pages/Login';
 import Wallet from './pages/Wallet';
 
-const store = createStore(rootReducer, composeWithDevTools());
-
 function App() {
   return (
-    <Provider store={ store }>
-      <div>
-        Hello, TrybeWallet!
-        <Routes>
-          <Route path="/" element={ <Login /> } />
-          <Route path="/carteira" element={ <Wallet /> } />
-        </Routes>
-      </div>
-    </Provider>
+    <div>
+      Hello, TrybeWallet!
+      <Routes>
+        <Route path="/" element={ <Login /> } />
+        <Route path="/carteira" element={ <Wallet /> } />
+      </Routes>
+    </div>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import store from './redux';
 
 ReactDOM
   .createRoot(document.getElementById('root') as HTMLElement)
   .render(
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>,
+    <Provider store={ store }>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+      ,
+    </Provider>,
   );
